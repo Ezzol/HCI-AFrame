905fbcf2da7ca5b8f8b0be174c72727ec015b8df
Ezzol <18054161@student.hhs.nl>
2021-05-13 14:35:18 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -4,7 +4,7 @@ var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x -= rotationSpeed;
-    myOtherBox.object3D.rotation.y -= rotationSpeed;
+    myOtherBox.object3D.rotation.y += rotationSpeed;
 
 	console.log(myOtherBox.object3D.rotation.x);
 }
